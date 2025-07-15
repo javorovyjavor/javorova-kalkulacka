4cd58543f6d0468ac88f48bdf0a0007768ddb5be
javorovyjavor <github@javor.info>
2025-07-15 16:38:15 +0200

    Add files via upload

---
+++ index_soubory/src_app_page_tsx_d846e3da._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_9818a52e._.js",
+  "static/chunks/node_modules_1191e093._.js"
+],
+    source: "dynamic"
+});
